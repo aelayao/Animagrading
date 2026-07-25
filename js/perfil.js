77809d7c28407c34fda259b90b09579938729c21
Astrid Astorga <astridastorga26@gmail.com>
2026-07-25 13:02:13 -0600

    progreso en pags. de ayuda y perfil

---
+++ js/perfil.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
+import { getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+import { getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+//identificadores de firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAWNa6Bk-nw5vbsR6izejn44V-C90bV2ik",
+  authDomain: "ad-c668f.firebaseapp.com",
+  projectId: "ad-c668f",
+  storageBucket: "ad-c668f.firebasestorage.app",
+  messagingSenderId: "478756211511",
+  appId: "1:478756211511:web:e2dcb8d9d070792d4168b2",
+  measurementId: "G-RW1BW7LCEB"
+};
+
+//Inicializar firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const db = getFirestore(app); //inicializa cloud firestore
+
+
+onAuthStateChanged(auth, (user) =>{
+	if (user) {
+		const uid = user.uid;
+		
+    const docRef = doc(db, "users", uid);
+    getDoc(docRef)
+    //mostrar graficamente la información del usuario:
+    .then(async (docSnap)=>{
+      if (docSnap.exists()){
+        const userData= docSnap.data();
+        document.getElementById('usrNombre').innerText=userData.nombre;
+        document.getElementById('usrMatr').innerText=userData.matricula;
+        document.getElementById('usrCorr').innerText=userData.email;
+      }
+      else
+      {
+        console.log("pero que ha pasao");
+      }
+    })
+    .catch((error)=>{
+      console.log("Error al obtener la información.");
+    })
+	}
+	else
+	{
+    console.log("Deslog")
+    window.location.href = "index.html";
+  }
+})
