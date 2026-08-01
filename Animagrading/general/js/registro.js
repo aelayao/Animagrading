4d2b82763f0f31622a437367a86503fc18f16bd1
Astrid Astorga <astridastorga26@gmail.com>
2026-07-31 22:15:40 -0600

    intento de verificar matricula unica idk

--- Animagrading/general/js/registro.js
+++ Animagrading/general/js/registro.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword, validatePassword} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
-import { getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, validatePassword } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+import { getFirestore, setDoc, collection, query, where, getDoc, doc } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 //identificadores de firebase
 const firebaseConfig = {
@@ -29,7 +29,7 @@ let pass = document.querySelector("#inpt_pass");
 let registrar = document.querySelector("#btn_regis");
 let grupo = document.querySelector("#slct_grupo");
 //reglas de regex? para la contraseñia: por lo menos una letra minus, un dígito, un caracter especial y de tamaño min 7
-let passlimit =  /^(?=.*[a-z])(?=.*\d)(?=.*[@.#$!%*?&]).{7,20}$/;
+let passlimit = /^(?=.*[a-z])(?=.*\d)(?=.*[@.#$!%*?&]).{7,20}$/;
 
 //otro regex para nombre y apellido
 let nomletr = /^[a-zA-ZáíúéóüñÁÍÚÉÓÜÑ]+$/;
@@ -45,13 +45,13 @@ registrar.addEventListener("click", (event) => {
   let gpo = grupo.value;
 
   //revisar si matrícula tiene 2, ya que así se manejan las matrículas de la unipoli (?)
-  if (!mat.startsWith('2')){
+  if (!mat.startsWith('2')) {
     alert("La matrícula debe de iniciar con el número 2. Por favor ingrese una matrícula válida.");
     return;
   }
 
   //revisar si matrícula es menor a 8 números
-  if(mat.length < 8){
+  if (mat.length < 8) {
     alert("La matrícula debe de tener 8 números.")
     return;
   }
@@ -64,22 +64,37 @@ registrar.addEventListener("click", (event) => {
   }
 
   //revisar que nombre y apellido sean escritos solo con letras
-  if(!nomletr.test(nom.trim())){
+  if (!nomletr.test(nom.trim())) {
     alert("Su nombre debe de ser ingresado solo con letras.")
     return;
   }
-  if(!nomletr.test(ap.trim())){
+  if (!nomletr.test(ap.trim())) {
     alert("Su nombre debe de ser ingresado solo con letras.")
     return;
   }
 
+  /*try{
+    //revisar si en usuarios hay alguien ya con una matrícula como la ingresada, aqui docRef no ocupa el uid (pq no hay nadie ingresado¿)
+  //no doc pq doc ocupa el uid!!
+  const notdocRef = collection(db, "users");
+  const matigl = query(notdocRef, where("matricula", "==", mat));
+  const resultao = await getDocs(matigl);
+
+  if (!resultao.empty) {
+    alert("Esta matrícula ya está registrada, ingresa una diferente. Si crees que ésto es un error, contactate con Animagrading.");
+    return;
+  }
+  }
+  catch{
+    console.log("auxilio")
+  }*/
 
   // autenticación correo y contraseña/login de cuenta
   createUserWithEmailAndPassword(auth, email, password)
     //registro exitoso https://www.youtube.com/watch?v=_Xczf06n6x0
     .then((userCredential) => {
       const user = userCredential.user;
-      const userData={
+      const userData = {
         nombre: nom,
         apellidoPaterno: ap,
         matricula: mat,
@@ -88,16 +103,16 @@ registrar.addEventListener("click", (event) => {
         maestro: false,
         photoURL: "https://icons.iconarchive.com/icons/custom-icon-design/silky-line-user/128/user2-edit-icon.png"
       }
-      
-      const docRef=doc(db, "users", user.uid);
-      setDoc(docRef, userData)
-      .then(()=>{
-        window.location.href = "/Animagrading/alumnos/paginas/inicio.html";
 
-      })
-      .catch((error) =>{
-        console.error("error en document", error);
-      })
+      const docRef = doc(db, "users", user.uid);
+      setDoc(docRef, userData)
+        .then(() => {
+          window.location.href = "/Animagrading/alumnos/paginas/inicio.html";
+
+        })
+        .catch((error) => {
+          console.error("error en document", error);
+        })
 
     })
     //error de registro
@@ -107,10 +122,10 @@ registrar.addEventListener("click", (event) => {
       const errorCode = error.code;
       const errorMessage = error.message;
 
-      if(errorCode=='auth/email-already-in-use'){
+      if (errorCode == 'auth/email-already-in-use') {
         alert('Correo ya registrado!');
       }
-      else{
+      else {
         alert('No se pudo crear usuario. Inténtelo de nuevo más tarde');
       }
     })
